3c4d0fbb2ce622abe0037ec86190df94aae6ce2c
jaredcohen1998 <32070535+jaredcohen1998@users.noreply.github.com>
2021-09-23 14:54:04 -0400

    CountVectorizer stuff

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import numpy as np
 import re
 import matplotlib.pyplot as plt
 import sklearn.datasets
+from sklearn.feature_extraction.text import CountVectorizer
 
 def main():
     list_of_files = sklearn.datasets.load_files('data\\BBC', encoding='latin1', load_content=True, random_state=0)
     print('Index of 1: ')
-    print(list_of_files['target_names'][1])
-    print(list_of_files['filenames'][1])
-    print(list_of_files['data'][1])
-    #word_frequency(list_of_files)
+    print(len(list_of_files['target_names']))
+    print(len(list_of_files['filenames']))
+    print(len(list_of_files['data']))
+
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(list_of_files['data'])
+    print(vectorizer.get_feature_names())
+    #print(X.toarray())
     
 if __name__ == "__main__":
     main()
